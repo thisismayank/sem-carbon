78a3a3ef375b6cbb4740c56fecead9c02b360016
Mayank Arya <mayank@getbasis.co>
2023-11-12 17:42:06 -0800

    Add library folder

---
+++ src/lib/response.js
@@ -0,0 +1,13 @@
+const response = {
+    APPLICATION_ERROR: {
+        SERVER_ERROR: {
+            success: false,
+            version: 1,
+            statusCode: "500_001",
+            message: "Server error.",
+            messageObj: {},
+        }
+    }
+}
+
+export default response;
