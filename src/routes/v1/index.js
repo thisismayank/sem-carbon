2769dded22421992a6a02b85810738921e5f35f7
Mayank Arya <mayank@getbasis.co>
2023-11-14 02:19:45 -0800

    Add configuration for carbon route

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,8 +1,12 @@
 import express from "express";
 import userRoutes from "./user.route";
+import carbonRoutes from "./carbon.route";
+
 
 const v1Router = express.Router();
 
 v1Router.use("/users", userRoutes);
+v1Router.use("/carbon", carbonRoutes);
+
 
 export default v1Router;
