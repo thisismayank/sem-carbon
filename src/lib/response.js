66edbd24e59d062f578b8158c5df0d08cd4e9dcd
Mayank Arya <mayank@getbasis.co>
2023-11-14 02:18:15 -0800

    Add relevant response structures

--- src/lib/response.js
+++ src/lib/response.js
@@ -7,6 +7,81 @@ const response = {
             message: "Server error.",
             messageObj: {},
         }
+    },
+    AUTH: {
+        GENERATE_OTP: {
+            EXISTING_USER: {
+                SUCCESS: {
+                    success: true,
+                    version: 1,
+                    statusCode: "200_000_002",
+                    message: "User logged in successfully.",
+                    results: {},
+                }
+            },
+            SEND_GENERATED_OTP: {
+                SUCCESS: {
+                    success: true,
+                    version: 1,
+                    statusCode: "200_000_002",
+                    message: "User logged in successfully.",
+                    results: {},
+                }
+            }
+        },
+        VERIFY_OTP: {
+            SUCCESS: {
+                success: true,
+                version: 1,
+                statusCode: "200_000_001",
+                message: "OTP verified successfully.",
+                results: {},
+            },
+            FAILURE: {
+                success: false,
+                version: 1,
+                statusCode: "500_000_001",
+                message: "Wrong OTP.",
+                messageObj: {},
+            }
+        },
+        CREATE_NEW_USER: {
+            SUCCESS: {
+                success: true,
+                version: 1,
+                statusCode: "200_000_001",
+                message: "User account created successfully.",
+                results: {},
+            },
+            FAILURE: {
+                success: false,
+                version: 1,
+                statusCode: "500_000_001",
+                message: "Could not create a new account.",
+                messageObj: {},
+            }
+        }
+    },
+
+    CARBON: {
+        CNAUGHT: {
+            PLACE_ORDER: {
+                SUCCESS: {
+                    success: true,
+                    version: 1,
+                    statusCode: "200_000_010",
+                    message: "Order placed successfully",
+                    results: {},
+                },
+                FAILURE: {
+                    success: false,
+                    version: 1,
+                    statusCode: "500_000_010",
+                    message: "Some error occured in order placing.",
+                    messageObj: {},
+                }
+            }
+        }
     }
 }
 
