f980f827b6afa0d94887cbd2aaf5fe660662a861
Mayank Arya <mayank@getbasis.co>
2023-11-14 02:17:30 -0800

    Add email token send and verification and new user sign up APIs

--- src/routes/v1/user.route.js
+++ src/routes/v1/user.route.js
@@ -1,12 +1,13 @@
 import express from "express";
-import * as controller from "../../controllers/user.controller.js";
+import * as controller from "../../controllers/auth.controller.js";
 const router = express.Router();
 
-router.route("/email").post(controller.sendEmailVerificationToken);
+router.route("/email").post(controller.sendTokenForEmailVerification);
 
-router.route("/email/verify").post();
+router.route("/email/verify").post(controller.verifyEmailVerificationToken);
 
-router.route("/").post();
+router.route("/").post(controller.createNewAccountForUser);
+
 
 export default router;
 
