3b88d2ea4d59f1aa6de991bf1e83cf05c522883d
Mayank Arya <mayank@getbasis.co>
2023-11-14 02:43:26 -0800

    Add authorize middleware in the place order call

--- src/routes/v1/carbon.route.js
+++ src/routes/v1/carbon.route.js
@@ -1,9 +1,11 @@
 import express from "express";
 import * as controller from "../../controllers/carbon.controller.js";
+
+import { authorize } from "../../middleware/authorize.js";
 const router = express.Router();
 
 
-router.route("/cn/").post(controller.placeOrder);
+router.route("/cn/").post(authorize, controller.placeOrder);
 
 
 export default router;
