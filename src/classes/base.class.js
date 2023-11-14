6a3d5d265a4c72104ebd36f6107f67f5384de462
Mayank Arya <mayank@getbasis.co>
2023-11-13 16:45:26 -0800

    Add base class

---
+++ src/classes/base.class.js
@@ -0,0 +1,14 @@
+import asyncRedis from "async-redis";
+
+import { redisConfig } from "../config";
+
+// Establishing redis connection
+const redisClient = asyncRedis.createClient(redisConfig.port, redisConfig.host);
+
+export default class BaseClass {
+    constructor(name, connection, redis) {
+        // if (redisClient) {
+        // this.redis = redisClient;
+        // }
+    }
+}
