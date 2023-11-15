a87812453ef9a8d32b06e4ba4797e009874fc39e
Mayank Arya <mayank@getbasis.co>
2023-11-15 05:33:55 -0800

    Add mongo invoke script to index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import http from "http";
 import app from "./config/express.config.js";
 import logger from "./lib/logger.js";
-
+import { connectMongoose } from "./lib/mongoose.js";
 
 const appServer = http.createServer(app);
 
 appServer.listen(3000, async () => {
     logger.info("INFO: Listening on port 3000");
+    await connectMongoose();
+
 })
