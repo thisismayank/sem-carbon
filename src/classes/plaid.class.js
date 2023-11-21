bac764d00774a8912ef7d0a1e60414a2213ab1f3
Mayank Arya <mayank@getbasis.co>
2023-11-21 06:07:55 -0800

    Add transaction and identity support to plaid apis

--- src/classes/plaid.class.js
+++ src/classes/plaid.class.js
@@ -22,7 +22,7 @@ export default class PlaidClass extends BaseClass {
     async createLinkToken(userId) {
         try {
             logger.info(
-                `INFO: AuthClass-createLinkToken - User ID: ${userId}`
+                `INFO: PlaidClass-createLinkToken - User ID: ${userId}`
             );
             const linkToken = await Plaid.createLinkToken(userId);
             console.log('PLAID CLASS ', linkToken)
@@ -42,7 +42,7 @@ export default class PlaidClass extends BaseClass {
                 }
             }
         } catch (error) {
-            logger.error(`ERROR: AuthClass-createLinkToken - ${error}`);
+            logger.error(`ERROR: PlaidClass-createLinkToken - ${error}`);
             throw error;
 
         }
@@ -50,7 +50,7 @@ export default class PlaidClass extends BaseClass {
     async exchangePublicToken(userId, publicToken) {
         try {
             logger.info(
-                `INFO: AuthClass-exchangePublicToken - User ID: ${userId} - Public Token: ${publicToken}`
+                `INFO: PlaidClass-exchangePublicToken - User ID: ${userId} - Public Token: ${publicToken}`
             );
             const publicTokenResponse = await Plaid.exchangePublicToken(publicToken);
 
@@ -70,7 +70,7 @@ export default class PlaidClass extends BaseClass {
                 }
             }
         } catch (error) {
-            logger.error(`ERROR: AuthClass-exchangePublicToken - ${error}`);
+            logger.error(`ERROR: PlaidClass-exchangePublicToken - ${error}`);
             throw error;
 
         }
@@ -79,7 +79,7 @@ export default class PlaidClass extends BaseClass {
     async getAccountData(userId, accessToken) {
         try {
             logger.info(
-                `INFO: AuthClass-getAccountData - User ID: ${userId} - Public Token: ${accessToken}`
+                `INFO: PlaidClass-getAccountData - User ID: ${userId} - Access Token: ${accessToken}`
             );
             const accountResponseData = await Plaid.getAccountData(accessToken);
             console.log('accountResponseData', accountResponseData)
@@ -100,7 +100,114 @@ export default class PlaidClass extends BaseClass {
                 }
             }
         } catch (error) {
-            logger.error(`ERROR: AuthClass-getAccountData - ${error}`);
+            logger.error(`ERROR: PlaidClass-getAccountData - ${error}`);
+            throw error;
+
+        }
+    }
+
+    async getTransactionData(userId, accessToken) {
+        try {
+            logger.info(
+                `INFO: PlaidClass-getTransactionData - User ID: ${userId} - Access Token: ${accessToken}`
+            );
+            const transactionData = await Plaid.getTransactionData(accessToken);
+            console.log('transactionData', transactionData)
+            if (!transactionData.success) {
+                return {
+                    ...response.CARBON.CNAUGHT.PLACE_ORDER.FAILURE,
+                    messageObj: {
+                        error: "Some error occured in order placement"
+                    }
+                }
+            }
+
+            return {
+                ...response.CARBON.CNAUGHT.PLACE_ORDER.SUCCESS,
+                results: {
+                    added: transactionData.transactionResponseData.added,
+                    has_more: transactionData.transactionResponseData.has_more,
+                    next_cursor: transactionData.transactionResponseData.next_cursor,
+
+                }
+            }
+        } catch (error) {
+            logger.error(`ERROR: PlaidClass-getTransactionData - ${error}`);
+            throw error;
+
+        }
+    }
+
+    async getRecurringTransactions(userId, accessToken) {
+        try {
+            logger.info(
+                `INFO: PlaidClass-getRecurringTransactions - User ID: ${userId} - Access Token: ${accessToken}`
+            );
+
+            const accountIds = ["jv7xArkxkRiAKLN5zqQxiZN4GMvXb9c6kJjKE"];
+            const recurringTransactionsData = await Plaid.getRecurringTransactions(accessToken, accountIds);
+            console.log('recurringTransactionsData', recurringTransactionsData)
+            if (!recurringTransactionsData.success) {
+                return {
+                    ...response.CARBON.CNAUGHT.PLACE_ORDER.FAILURE,
+                    messageObj: {
+                        error: "Some error occured in order placement"
+                    }
+                }
+            }
+
+            return {
+                ...response.CARBON.CNAUGHT.PLACE_ORDER.SUCCESS,
+                results: {
+                    deposits: recurringTransactionsData.transactionResponseData.inflow_streams,
+                    expenses: recurringTransactionsData.transactionResponseData.outflow_streams,
+                    lastUpdatedAt: recurringTransactionsData.transactionResponseData.updated_datetime,
+
+                }
+            }
+        } catch (error) {
+            logger.error(`ERROR: PlaidClass-getRecurringTransactions - ${error}`);
+            throw error;
+
+        }
+    }
+
+    async getIdentityInformation(userId, accessToken) {
+        try {
+            logger.info(
+                `INFO: PlaidClass-getIdentityInformation - User ID: ${userId} - Access Token: ${accessToken}`
+            );
+            const userIdentificationData = await Plaid.getIdentityInformation(accessToken);
+            console.log('userIdentificationData', JSON.stringify(userIdentificationData, null, 2))
+            if (!userIdentificationData.success) {
+                return {
+                    ...response.CARBON.CNAUGHT.PLACE_ORDER.FAILURE,
+                    messageObj: {
+                        error: "Some error occured in order placement"
+                    }
+                }
+            }
+            const responseData = {
+                name: userIdentificationData.userIdentificationResponseData.accounts[0].owners[0].names[0],
+                email: userIdentificationData.userIdentificationResponseData.accounts[0].owners[0].emails.filter(email => { if (email.primary) { return email.data } })[0].data,
+                phoneNumber: userIdentificationData.userIdentificationResponseData.accounts[0].owners[0].phone_numbers.filter(phoneNumber => { if (phoneNumber.type === "mobile") { return phoneNumber.data } })[0].data,
+                address: userIdentificationData.userIdentificationResponseData.accounts[0].owners[0].addresses.filter(address => { if (address.primary) { return address.data } })[0].data,
+                mask: userIdentificationData.userIdentificationResponseData.accounts[0].mask,
+                accountNameAlias: userIdentificationData.userIdentificationResponseData.accounts[0].name,
+                names: userIdentificationData.userIdentificationResponseData.accounts[0].owners[0].names,
+                officialAccountName: userIdentificationData.userIdentificationResponseData.accounts[0].official_name,
+                addresses: userIdentificationData.userIdentificationResponseData.accounts[0].owners[0].addresses,
+                emails: userIdentificationData.userIdentificationResponseData.accounts[0].owners[0].emails,
+                phoneNumbers: userIdentificationData.userIdentificationResponseData.accounts[0].owners[0].phone_numbers,
+            }
+            return {
+                ...response.CARBON.CNAUGHT.PLACE_ORDER.SUCCESS,
+                results: {
+                    ...responseData
+                }
+            }
+        } catch (error) {
+            logger.error(`ERROR: PlaidClass-getIdentityInformation - ${error}`);
             throw error;
 
         }
